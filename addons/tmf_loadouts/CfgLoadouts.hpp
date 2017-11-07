2f291dcbe3d91ce249fcec46351c81f8ea6d25c7
ryanparsons7 <ryanparsons7@gmail.com>
2017-11-07 17:09:57 +0000

    Added Categories to loadouts

--- addons/tmf_loadouts/CfgLoadouts.hpp
+++ addons/tmf_loadouts/CfgLoadouts.hpp
@@ -3,31 +3,37 @@ class CfgLoadouts
 	class rangers_90
     {
         displayName = "Rangers 90s";
+		category = "American";
         #include "loadouts\rangers90.hpp"
     };
     class delta_90
     {
         displayName = "Deltas 90s";
+		category = "American";
         #include "loadouts\delta90.hpp"
     };
     class middle_east_militia
     {
         displayName = "Middle East Militia";
+		category = "Middle East";
         #include "loadouts\middle_east_militia.hpp"
     };
     class un
     {
         displayName = "United Nations";
+		category = "Independents";
         #include "loadouts\un.hpp"
     };
     class baf_mtp
     {
         displayName = "British Armed Forces (MTP)";
+		category = "British";
         #include "loadouts\baf_mtp.hpp"
     };
     class ru_msv
     {
-        displayName = "Russian Federation MSV";
+        displayName = "Russian Federation MSV"
+		category = "Russian";
         #include "loadouts\ru_msv.hpp"
     };
 };
